a9f00b57ec1978b13bb0e7f7da9d356cf3916eb7
Eeishi <fnailu@gmail.com>
2024-07-17 16:44:27 +0700

    add navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:appwrite/models.dart'; // Pastikan ini sudah ada
-import 'package:flutter_travel_1/api.dart'; // Pastikan ini sudah ada
+import 'package:appwrite/models.dart'; // Pastikan ini adalah import yang benar untuk Document.
+import 'package:flutter_travel_1/api.dart'; // Pastikan di sini terdapat definisi AppWrite.init() dan AppWrite.getBusTickets().
 
 void main() {
   runApp(const MyApp());
-  AppWrite
-      .init(); // Pastikan inisialisasi AppWrite dilakukan dengan benar di dalam main()
+  AppWrite.init();
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Traveloka Style App',
+      title: 'Travel',
       theme: ThemeData(
-        primaryColor: Colors.deepPurple,
-        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.deepPurple)
-            .copyWith(secondary: Colors.deepOrange),
         appBarTheme: AppBarTheme(
-          backgroundColor: Colors.white,
-          foregroundColor: Colors.black,
-          iconTheme: const IconThemeData(color: Colors.black),
-          elevation: 0,
+          backgroundColor: Colors
+              .transparent, // Ubah latar belakang app bar menjadi transparan
         ),
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch:
+              Colors.blueGrey, // Gunakan warna biru abu-abu sebagai warna utama
+        ).copyWith(
+          secondary: Colors
+              .deepPurple, // Gunakan warna ungu dalam gradasi sebagai warna sekunder
+        ),
+        scaffoldBackgroundColor: Colors.blueGrey
+            .shade50, // Gunakan latar belakang berwarna biru abu-abu muda
       ),
-      home: const MyHomePage(title: 'Traveloka Style App'),
+      home: const MyHomePage(title: 'Travel'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  const MyHomePage({super.key, required this.title});
 
   final String title;
 
@@ -41,94 +44,160 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late Future<List<Document>?> _futureMusic;
-
-  @override
-  void initState() {
-    super.initState();
-    _futureMusic =
-        AppWrite.getData(); // Ganti dengan fungsi getData yang sesuai
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.notifications_rounded),
-            onPressed: () {
-              // Implement your notification action here
-            },
-          ),
-        ],
-      ),
-      body: FutureBuilder<List<Document>?>(
-        future: _futureMusic,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (snapshot.hasData && snapshot.data!.isNotEmpty) {
-            final musicData = snapshot.data!;
-            return _buildMusicList(musicData);
-          } else {
-            return Center(child: Text('No data available'));
-          }
-        },
-      ),
-    );
-  }
-
-  Widget _buildMusicList(List<Document> musicData) {
-    return ListView.builder(
-      itemCount: musicData.length,
-      itemBuilder: (context, index) {
-        final musicItem = musicData[index];
-        return Container(
-          margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(12),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.3),
-                spreadRadius: 2,
-                blurRadius: 5,
-                offset: const Offset(0, 2),
+    return FutureBuilder<List<Document>?>(
+      future: AppWrite.getData(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(widget.title),
+              actions: [
+                IconButton(
+                  icon: const Icon(
+                    Icons.notifications_rounded,
+                    size: 30.0,
+                    color: Colors.black,
+                  ),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+            body: Center(child: CircularProgressIndicator()),
+          );
+        } else if (snapshot.hasError) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(widget.title),
+              actions: [
+                IconButton(
+                  icon: const Icon(
+                    Icons.notifications_rounded,
+                    size: 30.0,
+                    color: Colors.black,
+                  ),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+            body: Center(child: Text('Error: ${snapshot.error}')),
+          );
+        } else if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+          final busTickets = snapshot.data!;
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(widget.title),
+              actions: [
+                IconButton(
+                  icon: const Icon(
+                    Icons.notifications_rounded,
+                    size: 30.0,
+                    color: Colors.black,
+                  ),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+            body: SingleChildScrollView(
+              padding: EdgeInsets.all(18),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Available Travel Tickets',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10),
+                  ListView.builder(
+                    itemCount: busTickets.length,
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemBuilder: (context, index) {
+                      final ticket = busTickets[index];
+                      return Card(
+                        margin: const EdgeInsets.only(bottom: 10),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        elevation: 5,
+                        child: ListTile(
+                          contentPadding: EdgeInsets.all(10),
+                          leading: CircleAvatar(
+                            backgroundColor: Colors.blueGrey,
+                            child: Icon(Icons.flight,
+                                color:
+                                    Colors.white), // Ganti ikon dengan pesawat
+                          ),
+                          title: Text(
+                            ticket.data['destination'] ?? 'No Destination',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(height: 5),
+                              Text(
+                                  'From: ${ticket.data['departure'] ?? 'Unknown'}'),
+                              Text(
+                                  'Price: ${ticket.data['price'] ?? 'Unknown'} \$'),
+                              Text('Date: ${ticket.data['date'] ?? 'Unknown'}'),
+                            ],
+                          ),
+                          trailing: Icon(Icons.arrow_forward_ios,
+                              color: Colors.blueGrey),
+                          onTap: () {
+                            // Tambahkan fungsionalitas onTap di sini
+                          },
+                        ),
+                      );
+                    },
+                  ),
+                ],
               ),
-            ],
-          ),
-          child: ListTile(
-            contentPadding:
-                const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-            leading: ClipRRect(
-              borderRadius: BorderRadius.circular(8),
-              child: Image.network(
-                musicItem.data['image_url'] ?? '',
-                width: 60,
-                height: 60,
-                fit: BoxFit.cover,
+            ),
+            bottomNavigationBar: BottomAppBar(
+              child: Container(
+                height: 50.0,
+                color: Colors.blueGrey, // Warna latar belakang navigasi bawah
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.home, color: Colors.white),
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.search, color: Colors.white),
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.person, color: Colors.white),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
               ),
             ),
-            title: Text(
-              musicItem.data['nama'] ?? 'No Name',
-              style: const TextStyle(fontWeight: FontWeight.bold),
+          );
+        } else {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(widget.title),
+              actions: [
+                IconButton(
+                  icon: const Icon(
+                    Icons.notifications_rounded,
+                    size: 30.0,
+                    color: Colors.black,
+                  ),
+                  onPressed: () {},
+                ),
+              ],
             ),
-            subtitle: Text(
-              musicItem.data['genre'] ?? 'No Genre',
-              style: const TextStyle(color: Colors.grey),
-            ),
-            trailing: IconButton(
-              icon: const Icon(Icons.play_circle_fill),
-              onPressed: () {
-                // Implement play action here
-              },
-            ),
-          ),
-        );
+            body: Center(child: Text('No data available')),
+          );
+        }
       },
     );
   }
